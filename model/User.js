bc8544ca17929e5c4f71ec5f0737d1dc5c10051d
unknown <surajitkarmakar171@gmail.com>
2025-06-06 04:22:27 -0400

    final complete

--- model/User.js
+++ model/User.js
@@ -2,36 +2,31 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
-    name: {
+  name: {
     type: String,
     required: true,
     trim: true
   },
-   email: {
+  email: {
     type: String,
     required: true,
     unique: true,
     lowercase: true
   },
-
-  password: {
-    type : String,
-    required: true,
-    unique : true,
+  password: { 
+    type: String,
+    required: true
   },
-
-  
   createdAt: {
     type: Date,
     default: Date.now
   }
-
 });
 
 
-userSchema.pre('save', async (next)=>{
+userSchema.pre('save', async function(next){
   const saltround = 10;
-  this.password = await bcrypt.hash(this.password, saltround);
+  this.password= await bcrypt.hash(this.password, saltround);
   next()
 });
 
--- model/validation.js
+++ model/validation.js
@@ -1,16 +1,11 @@
-import joi from "joi";
+import Joi from "joi";
 
-const uservalidation = joi.object({
-  name: joi.string().min(3).max(10).required(),
-
-  email: joi.string().email({ minDomainSegments: 2 }).required(),
-
-  password: joi.string()
-    .min(6)
-    .max(30)
-    .pattern(new RegExp("^[a-zA-Z0-9@#$%^&+=!]*$"))
-    .required(),
+const uservalidation = Joi.object({
+  name: Joi.string().min(3).max(50).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required()
 });
 
 export default uservalidation;
+
 
